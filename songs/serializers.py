b8eff96d81f4ae3f4c54ee79240a93f9e3137f81
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-25 15:40:22 -0400

    serializer creation

---
+++ songs/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Songs
+
+class SongSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = Songs
+        fields = ['title', 'artist', 'album', 'release date', 'genre']
+        
