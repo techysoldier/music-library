2c6033f66651a966e28c590b13666a129085a7db
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-25 15:33:49 -0400

    makemigrations +Songs app

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Songs (models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.DecimalField(max_digits=8, decimal_places=2)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=255)
+   
+
+   
