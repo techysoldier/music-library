7c3e4634ec6e1e3131996c3eed21765a5b3e40e7
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-25 16:16:10 -0400

    url update

---
+++ songs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.song_list),
+    # path('<int:pk>/', views.song_detail),
+]
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Songs
 
-# Create your views here.
+@api_view(['GET'])
+def song_list(request):
+      # if request.method == 'GET':
+      #   songs = Songs.objects.all()
+      #   serializer = SongSerializer(songs,many=True)
+        return Response('ok')
